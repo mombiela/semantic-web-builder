b8efd437dced948cc3dfce37cf3d97c600b95fbc
Joan Costa <mombiela@github.com>
2017-03-22 07:44:55 +0100

    Cambio en ficheros

---
+++ web/templates/page.js
@@ -0,0 +1,6 @@
+	function setLang(location, dest_lang)
+	{
+		var location = "../" + dest_lang + "/${doc_name}.html";
+		Cookies.set('lang', dest_lang, {expires: 365});
+		window.location.href = location;
+	}
