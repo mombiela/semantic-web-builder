c1e836a9028c7ceceb53999f448f41a5eb2c12b5
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2017-11-20 20:47:35 +0100

    M+

--- paypal/key.bat
+++
@@ -1,6 +0,0 @@
-openssl genrsa -out %1-prvkey.pem 1024
-openssl req -new -key %1-prvkey.pem -x509 -days 365 -out %1-pubcert.pem
-openssl pkcs12 -export -inkey %1-prvkey.pem -in %1-pubcert.pem -out %1-prvkey.p12
- 
-
-
